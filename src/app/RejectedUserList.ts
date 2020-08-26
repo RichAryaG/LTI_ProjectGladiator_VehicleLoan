8d0cee43d253e39a218f399a6892abf50695646a
RichAryaG <gajendrar.jnv@gmail.com>
2020-08-27 03:06:49 +0530

    Checking loan based on their type

--- src/app/RejectedUserList.ts
+++ src/app/RejectedUserList.ts
@@ -1,4 +1,18 @@
 export class RejectedUserList
 {
+    loanId: any;
+        applicationStatus: any;
+        emi: any;
+        interestRate: any;
+        loanAmount: any;
+        loanEndDate: any;
+        loanStartDate: any;
+        loanStatus: any;
+        processingFee: any;
+        tenure: any;
+        loantype: any;
+        accounts: any;
+        vehicle: any;
+        userTables: any;
     
 }
--- src/app/view-application/view-application.component.ts
+++ src/app/view-application/view-application.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../UserService';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-view-application',
@@ -7,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewApplicationComponent implements OnInit {
 
-  constructor() { }
+  public AllPendingUserList =[];
 
-  ngOnInit(): void {
-  }
+  constructor (private service:UserService, private router:Router) { }
+  
+  ngOnInit() {
+    this.service.viewPending()
+    .subscribe(data => this.AllPendingUserList = data);
 
+}
 }
