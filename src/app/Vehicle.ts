85795e5f3975d8d9eeb10b444a8ff5559aa92d3e
Kavita Karki <kvtkrk441@gmail.com>
2020-08-26 16:07:32 +0530

    Changes!

---
+++ src/app/Vehicle.ts
@@ -0,0 +1,11 @@
+export class Vehicle {
+    
+    vehicleId:string;
+    vehicleMake:string;
+    vehicleModel:string;
+    vehicleType:string;
+    vehicleYear:number;
+    vehicleColour:string;
+    exShowroomPrice:number;
+    onRoadPrice:number;
+}
