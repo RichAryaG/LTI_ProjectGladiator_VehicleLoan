30fbf6c0cace6019a1078641c5941c34d2e70a21
Kavita Karki <kvtkrk441@gmail.com>
2020-08-27 17:16:11 +0530

    EMI CHECK

--- src/app/loanregister/loanregister.component.ts
+++ src/app/loanregister/loanregister.component.ts
@@ -21,8 +21,43 @@ export class LoanregisterComponent implements OnInit {
   loanStatus:any;
   tenure:any;
   loantype:any;
-  selectedEMI: string = '23';
+  selectedEMI:string;
   emi=this.loanAmount/this.tenure;
+
+  selectChangeHandler() {
+      if(this.tenure==6)
+      {
+        this.selectedEMI="7%";
+      }
+      else if(this.tenure==12)
+      {
+        this.selectedEMI="7.5%";
+      }
+      else if(this.tenure==24)
+      {
+        this.selectedEMI="8%";
+      }
+      else if(this.tenure==36)
+      {
+        this.selectedEMI="8.5%";
+      }
+      else if(this.tenure==48)
+      {
+        this.selectedEMI="9%";
+      }
+      else if(this.tenure==60)
+      {
+        this.selectedEMI="9.5%";
+      }
+      else if(this.tenure==72)
+      {
+        this.selectedEMI="10.5%";
+      }
+      else if(this.tenure==84)
+      {
+        this.selectedEMI="11%";
+      }
+  } 
 
   registerloan()
   {
@@ -30,7 +65,6 @@ export class LoanregisterComponent implements OnInit {
     {
       this.interestRate=7;
     }
-
     else if(this.tenure==12)
     {
       this.interestRate=7.5;
@@ -59,12 +93,23 @@ export class LoanregisterComponent implements OnInit {
     {
       this.interestRate=11;
     }
+
+    if(this.loantype=2){
+      this.processingFee = 2000;
+    }
+    else if (this.processingFee = 3) {
+      this.processingFee = 3000;
+    }
+    else if (this.processingFee = 4) {
+      this.processingFee = 4000;
+    }
+    console.log(this.processingFee);
     var loan=new Loan();
     loan.applicationStatus="Pending";
     loan.interestRate=this.interestRate;
     loan.loanAmount=this.loanAmount;
     loan.loantype=this.loantype;
-    loan.processingFee=2000;
+    loan.processingFee=this.processingFee;
     loan.loanEndDate=new Date(new Date().setMonth(new Date().getMonth() + Number(this.tenure)));
     loan.loanStartDate=formatDate(new Date(), 'yyyy-MM-dd', 'en');
     loan.loanStatus="New";
@@ -75,22 +120,22 @@ export class LoanregisterComponent implements OnInit {
         if(loan.status=='SUCCESS')
         {
           console.log(loan)
-          alert("New Loan Registered !!");
+          alert("New Loan Registered!!");
           this.router.navigate(['loginsuccess'])
         }
         else
         {
-          alert("Loan Already Exists !!");
+          alert("Loan Already Exists!!");
         }
     }
     )
   }
 
   ngOnInit(): void {
-    // this.userId=sessionStorage.getItem('userId');
-    // if(this.userId==null)
-    // {
-    //   this.router.navigate(['user']);
-    // }
+    this.userId=sessionStorage.getItem('userId');
+    if(this.userId==null)
+    {
+      this.router.navigate(['user']);
+    }
   }
 }
